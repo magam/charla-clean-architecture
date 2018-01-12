391da328993f2a49f5bc3416a3a5de2045b4f121
gacevedo <acevedo.gaston@gmail.com>
2018-01-12 13:28:45 -0300

    Primer test

---
+++ domain/src/test/kotlin/com/fravega/ecommerce/poker/test/unit/hands/HandOfPairTest.kt
@@ -0,0 +1,27 @@
+package com.fravega.ecommerce.poker.test.unit.hands
+
+import org.junit.Test
+
+internal class HandOfPairTest {
+
+    @Test
+    fun aPairShouldBeResolved(){
+        givenAHandWithAPair()
+
+        whenResolveHandValue()
+
+        thenAPairIsResolved()
+    }
+
+    private fun givenAHandWithAPair() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun whenResolveHandValue() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun thenAPairIsResolved() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
